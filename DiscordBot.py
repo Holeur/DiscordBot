b1ab729ea22796c557a8ec945d4ffe29491d9fd5
Vladimir <mulkov-01@yandex.ru>
2021-05-31 03:58:30 +0500

    Update DiscordBot.py

--- DiscordBot.py
+++ DiscordBot.py
@@ -26,7 +26,13 @@ try:
     active_channel_id = ""
     main_guild = ""
     
-    browser = webdriver.Firefox()
+
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     
     def timelog():
         return time.ctime(time.time())
